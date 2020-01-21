9a627212ca9d66d1295afb16d4f904d5a0c270c0
GD <gautier.dulac@polytechnique.Edu>
2020-01-21 18:58:54 +0100

    UPDATE - Format

--- rlcard/agents/random_agent.py
+++ rlcard/agents/random_agent.py
@@ -2,32 +2,32 @@ import numpy as np
 
 
 class RandomAgent(object):
-    ''' A random agent. Random agents is for running toy examples on the card games
-    '''
+    """ A random agent. Random agents is for running toy examples on the card games
+    """
 
     def __init__(self, action_num):
-        ''' Initilize the random agent
+        """ Initilize the random agent
 
         Args:
             action_num (int): the size of the ouput action space
-        '''
+        """
         self.action_num = action_num
 
     @staticmethod
     def step(state):
-        ''' Predict the action given the curent state in gerenerating training data.
+        """ Predict the action given the curent state in gerenerating training data.
 
         Args:
             state (numpy.array): an numpy array that represents the current state
 
         Returns:
             action (int): the action predicted (randomly chosen) by the random agent
-        '''
-        #return np.random.randint(0, self.action_num)
+        """
+        # return np.random.randint(0, self.action_num)
         return np.random.choice(state['legal_actions'])
 
     def eval_step(self, state):
-        ''' Predict the action given the curent state for evaluation.
+        """ Predict the action given the curent state for evaluation.
             Since the random agents are not trained. This function is equivalent to step function
 
         Args:
@@ -35,5 +35,5 @@ class RandomAgent(object):
 
         Returns:
             action (int): the action predicted (randomly chosen) by the random agent
-        '''
+        """
         return self.step(state)

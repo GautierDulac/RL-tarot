b56a3926cfc2e7d7e819e597c51c5d5ea69f57d8
GD <gautier.dulac@polytechnique.Edu>
2020-01-21 18:28:10 +0100

    UPDATE - Format

--- rlcard/models/__init__.py
+++ rlcard/models/__init__.py
@@ -1,8 +1,8 @@
-''' Register rule-based models or pre-trianed models
-'''
+""" Register rule-based models or pre-trianed models
+"""
 
 from rlcard.models.registration import register, load
 
 register(
-    model_id = 'tarot-rule-v1',
-    entry_point='rlcard.models.uno_rule_models:UNORuleModelV1')
+    model_id='tarot-rule-v1',
+    entry_point='rlcard.models.tarot_rule_models:TarotRuleModelV1')

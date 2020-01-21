59434f33e6855ae17067c28f140e32969c56d9a3
GD <gautier.dulac@polytechnique.Edu>
2020-01-21 13:57:32 +0100

    COMPLETE - Adaptation done

--- rlcard/games/tarot/dealer.py
+++ rlcard/games/tarot/dealer.py
@@ -1,27 +1,31 @@
+# CLEAR
+
+
 import random
 
 from rlcard.games.tarot.utils import init_deck
+from rlcard.games.tarot.player import TarotPlayer
 
 
 class TarotDealer(object):
-    ''' Initialize a tarot dealer class
-    '''
+    """ Initialize a tarot dealer class
+    """
+
     def __init__(self):
         self.deck = init_deck()
         self.shuffle()
 
     def shuffle(self):
-        ''' Shuffle the deck
-        '''
+        """ Shuffle the deck
+        """
         random.shuffle(self.deck)
 
-    def deal_cards(self, player, num):
-        ''' Deal some cards from deck to one player
+    def deal_cards(self, player: TarotPlayer, num):
+        """ Deal some cards from deck to one player
 
         Args:
             player (object): The object of DoudizhuPlayer
             num (int): The number of cards to be dealed
-        '''
+        """
         for _ in range(num):
             player.hand.append(self.deck.pop())
-

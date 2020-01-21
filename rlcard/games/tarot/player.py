7b3d5ff03b3271386735243c99d50b0f4a5c4829
GD <gautier.dulac@polytechnique.Edu>
2020-01-21 14:57:56 +0100

    COMPLETE - Adaptation done

--- rlcard/games/tarot/player.py
+++ rlcard/games/tarot/player.py
@@ -1,18 +1,22 @@
+# CLEAR
+
 
 class TarotPlayer(object):
 
-    def __init__(self, player_id):
-        ''' Initilize a player.
+    def __init__(self, player_id, asking=False):
+        """ Initilize a player.
 
         Args:
             player_id (int): The id of the player
-        '''
+        """
         self.player_id = player_id
         self.hand = []
         self.points = []
+        self.bouts = []
+        self.asking = asking
 
     def get_player_id(self):
-        ''' Return the id of the player
-        '''
+        """ Return the id of the player
+        """
 
         return self.player_id

757c8847b1b910faceea66e623d2e2ce02a4f49f
GD <gautier.dulac@polytechnique.Edu>
2020-01-21 12:44:47 +0100

    ADAPT - First changes in round management - still todo

--- rlcard/games/tarot/round.py
+++ rlcard/games/tarot/round.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-from rlcard.games.uno.card import UnoCard
-from rlcard.games.uno.utils import cards2list, WILD, WILD_DRAW_4
+from rlcard.games.tarot.card import TarotCard
+from rlcard.games.tarot.utils import cards2list, WILD, WILD_DRAW_4
 
 
-class UnoRound(object):
+class TarotRound(object):
 
     def __init__(self, dealer, num_players):
         ''' Initialize the round class
 
         Args:
-            dealer (object): the object of UnoDealer
+            dealer (object): the object of TarotDealer
             num_players (int): the number of players in game
         '''
         self.dealer = dealer
@@ -22,41 +22,12 @@ class UnoRound(object):
         self.is_over = False
         self.winner = None
 
-    def flip_top_card(self):
-        ''' Flip the top card of the card pile
-
-        Returns:
-            (object of UnoCard): the top card in game
-
-        '''
-        top = self.dealer.flip_top_card()
-        if top.trait == 'wild':
-            top.color = np.random.choice(UnoCard.info['color'])
-        self.target = top
-        self.played_cards.append(top)
-        return top
-
-    def perform_top_card(self, players, top_card):
-        ''' Perform the top card
-
-        Args:
-            players (list): list of UnoPlayer objects
-            top_card (object): object of UnoCard
-        '''
-        if top_card.trait == 'skip':
-            self.current_player = 1
-        elif top_card.trait == 'reverse':
-            self.direction = -1
-            self.current_player = (0 + self.direction) % self.num_players
-        elif top_card.trait == 'draw_2':
-            player = players[self.current_player]
-            self.dealer.deal_cards(player, 2)
-
     def proceed_round(self, players, action):
+        # TODO : adapt for TAROT
         ''' Call other Classes's functions to keep one round running
 
         Args:
-            player (object): object of UnoPlayer
+            player (object): object of TarotPlayer
             action (str): string of legal action
         '''
         if action == 'draw':
@@ -94,6 +65,7 @@ class UnoRound(object):
             self._preform_non_number_action(players, card)
 
     def get_legal_actions(self, players, player_id):
+        # TODO : Adapt for TAROT
         wild_flag = 0
         wild_draw_4_flag = 0
         legal_actions = []
@@ -103,7 +75,7 @@ class UnoRound(object):
         if target.type == 'wild':
             for card in hand:
                 if card.type == 'wild':
-                    #card.color = np.random.choice(UnoCard.info['color'])
+                    # card.color = np.random.choice(TarotCard.info['color'])
                     if card.trait == 'wild_draw_4':
                         if wild_draw_4_flag == 0:
                             wild_draw_4_flag = 1
@@ -115,7 +87,7 @@ class UnoRound(object):
                 elif card.color == target.color:
                     legal_actions.append(card.str)
 
-        # target is aciton card or number card
+        # target is action card or number card
         else:
             for card in hand:
                 if card.type == 'wild':
@@ -136,10 +108,11 @@ class UnoRound(object):
         return legal_actions
 
     def get_state(self, players, player_id):
+        # TODO : Adapt for TAROT
         ''' Get player's state
 
         Args:
-            players (list): The list of UnoPlayer
+            players (list): The list of TarotPlayer
             player_id (int): The id of the player
         '''
         state = {}
@@ -154,77 +127,3 @@ class UnoRound(object):
         state['others_hand'] = cards2list(others_hand)
         state['legal_actions'] = self.get_legal_actions(players, player_id)
         return state
-
-    def replace_deck(self):
-        ''' Add cards have been played to deck
-        '''
-        self.dealer.deck.extend(self.played_cards)
-        self.dealer.shuffle()
-        self.played_cards = []
-
-    def _perform_draw_action(self, players):
-        # replace deck if there is no card in draw pile
-        if not self.dealer.deck:
-            self.replace_deck()
-            #self.is_over = True
-            #self.winner = UnoJudger.judge_winner(players)
-            #return None
-
-        card = self.dealer.deck.pop()
-
-        # draw a wild card
-        if card.type == 'wild':
-            card.color = np.random.choice(UnoCard.info['color'])
-            self.target = card
-            self.played_cards.append(card)
-            self.current_player = (self.current_player + self.direction) % self.num_players
-
-        # draw a card with the same color of target
-        elif card.color == self.target.color:
-            if card.type == 'number':
-                self.target = card
-                self.played_cards.append(card)
-                self.current_player = (self.current_player + self.direction) % self.num_players
-            else:
-                self.played_cards.append(card)
-                self._preform_non_number_action(players, card)
-
-        # draw a card with the diffrent color of target
-        else:
-            players[self.current_player].hand.append(card)
-            self.current_player = (self.current_player + self.direction) % self.num_players
-
-    def _preform_non_number_action(self, players, card):
-        current = self.current_player
-        direction = self.direction
-        num_players = self.num_players
-
-        # perform reverse card
-        if card.trait == 'reverse':
-            self.direction = -1 * direction
-
-        # perfrom skip card
-        elif card.trait == 'skip':
-            current = (current + direction) % num_players
-
-        # perform draw_2 card
-        elif card.trait == 'draw_2':
-            if len(self.dealer.deck) < 2:
-                self.replace_deck()
-                #self.is_over = True
-                #self.winner = UnoJudger.judge_winner(players)
-                #return None
-            self.dealer.deal_cards(players[(current + direction) % num_players], 2)
-            current = (current + direction) % num_players
-
-        # perfrom wild_draw_4 card
-        elif card.trait == 'wild_draw_4':
-            if len(self.dealer.deck) < 4:
-                self.replace_deck()
-                #self.is_over = True
-                #self.winner = UnoJudger.judge_winner(players)
-                #return None
-            self.dealer.deal_cards(players[(current + direction) % num_players], 4)
-            current = (current + direction) % num_players
-        self.current_player = (current + self.direction) % num_players
-        self.target = card

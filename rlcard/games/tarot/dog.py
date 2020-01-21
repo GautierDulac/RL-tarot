7a269bdda4c05b3c322615847ee15d5d9e094c5f
GD <gautier.dulac@polytechnique.Edu>
2020-01-21 13:59:35 +0100

    COMPLETE - Adaptation done

--- rlcard/games/tarot/dog.py
+++ rlcard/games/tarot/dog.py
@@ -1,8 +1,9 @@
+# CLEAR
+
 
 class TarotDog(object):
 
     def __init__(self):
-        ''' Initilize a player.
-        '''
-        self.hand = []
-
+        """ Initilize a player.
+        """
+        self.dog_cards = []

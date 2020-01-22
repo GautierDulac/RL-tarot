eba779f3eb193d4933b13ef02c3f2b46337ac254
GD <gautier.dulac@polytechnique.Edu>
2020-01-22 16:34:16 +0100

    UPDATE unittest for TarotEnv

--- tests/envs/test_registration.py
+++ tests/envs/test_registration.py
@@ -7,17 +7,18 @@ from rlcard.envs.registration import register, make
 class TestRegistration(unittest.TestCase):
 
     def test_register(self):
-        register(env_id='test_reg', entry_point='rlcard.envs.blackjack:BlackjackEnv')
+        register(env_id='test_reg', entry_point='rlcard.envs.tarot:TarotEnv')
         with self.assertRaises(ValueError):
-            register(env_id='test_reg', entry_point='rlcard.envs.blackjack:BlackjackEnv')
+            register(env_id='test_reg', entry_point='rlcard.envs.tarot:TarotEnv')
 
     def test_make(self):
-        register(env_id='test_make', entry_point='rlcard.envs.blackjack:BlackjackEnv')
+        register(env_id='test_make', entry_point='rlcard.envs.tarot:TarotEnv')
         env = rlcard.make('test_make')
         _, player = env.init_game()
         self.assertEqual(player, 0)
         with self.assertRaises(ValueError):
             make('test_random_make')
+
 
 if __name__ == '__main__':
     unittest.main()

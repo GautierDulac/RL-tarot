e8071158ad6839ba98ccec65deee3cd8dfc246ff
GD <gautier.dulac@polytechnique.Edu>
2020-01-22 16:54:03 +0100

    UPDATE - unittest correction

--- tests/utils/test_logger.py
+++ tests/utils/test_logger.py
@@ -4,6 +4,7 @@ import shutil
 
 from rlcard.utils.logger import Logger
 
+
 class TestLogger(unittest.TestCase):
 
     def test_log(self):
@@ -17,6 +18,7 @@ class TestLogger(unittest.TestCase):
         contents = f.read()
         self.assertEqual(contents, "test text\n")
         logger.close_file()
+        f.close()
         shutil.rmtree(log_dir)
 
     def test_add_point(self):
@@ -30,8 +32,8 @@ class TestLogger(unittest.TestCase):
     def test_make_plot(self):
         logger = Logger(xlabel="x", ylabel="y", legend="test")
         for x in range(10):
-            logger.add_point(x=x, y=x*x)
-        self.assertEqual(9*9, logger.ys[9])
+            logger.add_point(x=x, y=x * x)
+        self.assertEqual(9 * 9, logger.ys[9])
         save_path = './newtest/test.png'
         save_dir = os.path.dirname(save_path)
         if os.path.exists(save_dir):
@@ -41,9 +43,11 @@ class TestLogger(unittest.TestCase):
         shutil.rmtree(save_dir)
 
     def test_close_file(self):
-        logger = Logger(xlabel="x", ylabel="y", legend="test", log_path="./newtest/test_log.txt",csv_path="./newtest/test_csv.csv")
+        logger = Logger(xlabel="x", ylabel="y", legend="test", log_path="./newtest/test_log.txt",
+                        csv_path="./newtest/test_csv.csv")
         logger.close_file()
         self.assertTrue(os.path.exists('./newtest/'))
+
 
 if __name__ == '__main__':
     unittest.main()

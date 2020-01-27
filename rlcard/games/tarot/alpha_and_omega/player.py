573071640c829b1d55b1115fd9d190622ca1d1bc
GD <gautier.dulac@polytechnique.Edu>
2020-01-27 16:46:32 +0100

    COMMENT - Precise function's documentation / typing

--- rlcard/games/tarot/alpha_and_omega/player.py
+++ rlcard/games/tarot/alpha_and_omega/player.py
@@ -1,20 +1,16 @@
-# CLEAR
-
-
 class TarotPlayer(object):
 
-    def __init__(self, player_id, taking=False):
-        """ Initilize a player.
-
-        Args:
-            player_id (int): The id of the player
+    def __init__(self, player_id: int):
+        """
+        Initialize a TarotPlayer object
+        :param player_id: int
         """
         self.player_id = player_id
         self.hand = []
         self.points = 0
         self.bouts = 0
         self.bid = None
-        self.taking = taking
+        self.taking = False
 
     def get_player_id(self):
         """ Return the id of the player

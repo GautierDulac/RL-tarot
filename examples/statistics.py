9af40ada3aaa0fc4e7803dd4aadadde91c927408
GD <gautier.dulac@polytechnique.Edu>
2020-02-04 11:48:43 +0000

    ADD - Framework to analyse some statistics on AI behaviors For now, looking at the bids depending on the hand value

--- examples/statistics.py
+++ examples/statistics.py
@@ -8,15 +8,18 @@ import tensorflow as tf
 import rlcard
 from rlcard.games.tarot.utils import get_hand_value, get_nb_bouts
 from rlcard.models.pretrained_models_tarot_v1 import TarotDQNModelV1
+from rlcard.models.pretrained_models_tarot_v4 import TarotDQNModelV4
 from rlcard.utils.logger import Logger
 
 num_tests = 100000
 log_limit = 1000
-stats_on_model = 1
+stats_on_model = 4
 
 save_path = 'examples/statistics/tarot_v{}/'.format(str(stats_on_model))
 log_path_taking = save_path + 'log.txt'
 csv_path_taking = save_path + 'taking_stats.csv'
+
+models = {'1': TarotDQNModelV1, '4': TarotDQNModelV4}
 
 # Make environment
 env = rlcard.make('tarot')
@@ -34,12 +37,14 @@ logger_taking = Logger(xlabel='hand_value', ylabel='nb_bouts', zlabel='taking_bi
 with tf.compat.v1.Session() as sess:
     # Set agents
     global_step = tf.Variable(0, name='global_step', trainable=False)
-    agent = TarotDQNModelV1(sess.graph, sess).dqn_agent
+    agent = models[str(stats_on_model)](sess.graph, sess).dqn_agent
     sess.run(tf.compat.v1.global_variables_initializer())
 
     records = [[], [], []]
     # STATS ON TAKING BID FOR FIRST PLAYER TO SPEAK
     for i in range(num_tests):
+        if i * 100 % num_tests == 0:
+            print('\rProgress: {}%'.format(int(i * 100 / num_tests)), end='')
         state, player_id = env.init_game()
         points_in_hand = get_hand_value(env.game.players[player_id].hand)
         records[0].append(points_in_hand)

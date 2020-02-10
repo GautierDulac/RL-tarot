f5a41b5fcac5a18fa002bf6607cad278b21f437b
GD <gautier.dulac@polytechnique.Edu>
2020-02-10 13:01:46 +0100

    ADD - Stats on model 100 - Temporary

--- rlcard/models/pretrained_models_tarot_v_.py
+++ rlcard/models/pretrained_models_tarot_v_.py
@@ -167,6 +167,56 @@ class TarotDQNModelV9(Model):
         """
         return False
 
+
+class TarotDQNModelV100(Model):
+    """ A pretrained model on Tarot with DQN
+    """
+
+    def __init__(self, graph, sess):
+        """ Load pretrained model
+        """
+        super().__init__()
+        self.graph = graph
+        self.sess = sess
+
+        env = rlcard.make('tarot')
+        with self.graph.as_default():
+            self.dqn_agent = DQNAgent(self.sess,
+                                      scope='dqn',
+                                      action_num=78,  # env.action_num,
+                                      replay_memory_size=20000,
+                                      replay_memory_init_size=memory_init_size,
+                                      norm_step=norm_step,
+                                      state_shape=env.state_shape,
+                                      mlp_layers=[512, 512])
+            normalize(env, self.dqn_agent, 1000)
+            self.sess.run(tf.compat.v1.global_variables_initializer())
+
+        check_point_path = os.path.join(ROOT_PATH, 'tarot_v100')
+        with self.sess.as_default():
+            with self.graph.as_default():
+                saver = tf.compat.v1.train.Saver(tf.compat.v1.model_variables())
+                saver.restore(self.sess, tf.train.latest_checkpoint(check_point_path))
+
+    @property
+    def agents(self) -> Union[DQNAgent, List[DQNAgent]]:
+        """
+         Get a list of agents for each position in a the game
+        :return: agents (list): A list of agents or an agent
+        Note: Each agent should be just like RL agent with step and eval_step
+              functioning well.
+        """
+        return self.dqn_agent
+
+    @property
+    def use_raw(self) -> bool:
+        """
+        Indicate whether use raw state and action
+        :return: (boolean): True if using raw state and action
+        """
+        return False
+
+
 def normalize(e: Env, agents: Union[DQNAgent, List[DQNAgent]], num: int) -> None:
     """
     Feed random data to normalizer

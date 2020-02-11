389d8a2f5ad72dece60dbdb8c4d663827ab7f2d1
GD <gautier.dulac@polytechnique.Edu>
2020-02-11 10:34:58 +0100

    ADD - First tentative self-played training

---
+++ examples/tarot_dqn_self_played.py
@@ -0,0 +1,143 @@
+"""
+An example of learning a Deep-Q Agent on French Tarot Game
+"""
+import os
+import time
+
+import tensorflow as tf
+
+import rlcard
+from rlcard.models.pretrained_models_tarot_v_ import TarotDQNModelV1
+from rlcard.utils.logger import Logger
+from rlcard.utils.utils import set_global_seed, time_difference_good_format
+
+record_number = 1
+starting_model = 1
+models = {'1': TarotDQNModelV1}
+
+# Make environment
+env = rlcard.make('tarot')
+eval_env = rlcard.make('tarot')
+
+# Set the iterations numbers and how frequently we evaluate/save plot
+evaluate_every = 100
+# save_plot_every = 100
+evaluate_num = 10000
+
+episode_num = 10000
+
+self_play = 1
+total_self_play_eval = int(episode_num / evaluate_every)
+
+# Set the the number of steps for collecting normalization statistics
+# and intial memory size
+memory_init_size = 1000
+norm_step = 1000
+
+# The paths for saving the logs and learning curves
+root_path = './experiments/tarot_dqn_self_played_v{}/'.format(str(record_number))
+log_path = root_path + 'log.txt'
+csv_path = root_path + 'performance.csv'
+figure_path = root_path + 'figures/'
+
+# Model save path
+if not os.path.exists('rlcard/models'):
+    os.makedirs('rlcard/models')
+    if not os.path.exists('rlcard/models/pretrained'):
+        os.makedirs('rlcard/models/pretrained')
+        for self_play in range(1, total_self_play_eval + 1):
+            if not os.path.exists(
+                    'rlcard/models/pretrained/tarot_v' + str(record_number * total_self_play_eval + self_play)):
+                os.makedirs('rlcard/models/pretrained/tarot_v' + str(record_number * total_self_play_eval + self_play))
+model_path = 'rlcard/models/pretrained/tarot_v' + str(record_number * total_self_play_eval + self_play) + '/model'
+
+# Set a global seed
+set_global_seed(42)
+
+with tf.compat.v1.Session() as sess:
+    # Set agents
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    agent = models[str(starting_model)](sess.graph, sess).dqn_agent
+
+    opponent_agent = agent
+
+    sess.run(tf.compat.v1.global_variables_initializer())
+
+    saver = tf.compat.v1.train.Saver()
+
+    env.set_agents([agent] + [opponent_agent] * (env.player_num - 1))
+    eval_env.set_agents([agent] + [opponent_agent] * (env.player_num - 1))
+
+    # Count the number of steps
+    step_counter = 0
+
+    # Init a Logger to plot the learning curve
+    logger = Logger(xlabel='timestep', ylabel='reward', legend='DQN on TAROT',
+                    legend_hist='Histogram of last evaluations', log_path=log_path, csv_path=csv_path)
+
+    total_game_played = 0
+    seconds = time.time()
+
+    for episode in range(episode_num):
+        print('\rEPISODE {} - Number of game played {} - {}'.format(episode, total_game_played,
+                                                                    time_difference_good_format(seconds, time.time())),
+              end='')
+
+        # Generate data from the environment
+        trajectories, _ = env.run(is_training=True)
+        total_game_played += 1
+
+        # Feed transitions into agent memory, and train the agent
+        for ts in trajectories[0]:
+            agent.feed(ts)
+            step_counter += 1
+
+            # Train the agent
+            train_count = step_counter - (memory_init_size + norm_step)
+            if train_count > 0:
+                loss = agent.train()
+                # print('\rINFO - Step {}, loss: {}'.format(step_counter, loss), end='')
+
+        # Evaluate the performance. Play with random agents.
+        if episode % evaluate_every == 0:
+            # Save Model
+            model_path = 'rlcard/models/pretrained/tarot_v' + str(
+                record_number * total_self_play_eval + self_play) + '/model'
+
+            saver.save(sess, model_path)
+            reward = 0
+            reward_list = []
+            for eval_episode in range(evaluate_num):
+                print('\rEPISODE {} - Eval {} over {} - Number of game played {} - {}'.format(episode, eval_episode,
+                                                                                              evaluate_num,
+                                                                                              total_game_played,
+                                                                                              time_difference_good_format(
+                                                                                                  seconds,
+                                                                                                  time.time())),
+                      end='')
+                _, payoffs = eval_env.run(is_training=False)
+                total_game_played += 1
+                reward_list.append(payoffs[0])
+                reward += payoffs[0]
+
+            logger.log('\n########## Evaluation - Episode {} ##########'.format(episode))
+            logger.log('Timestep: {} Average reward is {}'.format(env.timestep, float(reward) / evaluate_num))
+
+            # Add point to logger
+            logger.add_point(x=env.timestep, y=float(reward) / evaluate_num)
+
+            # Make plot
+            logger.make_plot(save_path=figure_path + str(episode) + '.png')
+            logger.make_plot_hist(save_path_1=figure_path + str(episode) + '_hist.png',
+                                  save_path_2=figure_path + str(episode) + '_freq.png', reward_list=reward_list)
+
+            self_play += 1
+
+            opponent_agent = agent
+            env.set_agents([agent] + [opponent_agent] * (env.player_num - 1))
+            eval_env.set_agents([agent] + [opponent_agent] * (env.player_num - 1))
+
+    # Make the final plot
+    logger.make_plot(save_path=figure_path + 'final_' + str(episode) + '.png')
+    logger.make_plot_hist(save_path_1=figure_path + str(episode) + '_hist.png',
+                          save_path_2=figure_path + str(episode) + '_freq.png', reward_list=reward_list)

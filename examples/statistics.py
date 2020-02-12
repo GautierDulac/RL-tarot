49ba666890a1c79d03045ebb00d64cc05687d1e2
GD <gautier.dulac@polytechnique.Edu>
2020-02-12 10:44:20 +0100

    ADD - Get statistics on model v60073

--- examples/statistics.py
+++ examples/statistics.py
@@ -8,15 +8,15 @@ import tensorflow as tf
 import rlcard
 from rlcard.agents.random_agent import RandomAgent
 from rlcard.games.tarot.utils import get_hand_value, get_nb_bouts
-from rlcard.models.pretrained_models_tarot_v_ import TarotDQNModelV1, TarotDQNModelV10018
+from rlcard.models.pretrained_models_tarot_v_ import TarotDQNModelV1, TarotDQNModelV60073
 from rlcard.utils.logger import Logger
 
 num_tests = 100000
-num_games = 25000
-stats_on_model = 10018
+num_games = 10000
+stats_on_model = 60073
 self_played = True
 self_record_number = 1
-models = {'0': RandomAgent, '1': TarotDQNModelV1, '10018': TarotDQNModelV10018}
+models = {'0': RandomAgent, '1': TarotDQNModelV1, '60073': TarotDQNModelV60073}
 
 # Model save path
 save_path = 'examples/statistics/tarot_v{}/'.format(str(stats_on_model))
--- rlcard/models/pretrained_models_tarot_v_.py
+++ rlcard/models/pretrained_models_tarot_v_.py
@@ -139,11 +139,11 @@ class TarotDQNModelV60073(Model):
                                       replay_memory_init_size=memory_init_size,
                                       norm_step=norm_step,
                                       state_shape=env.state_shape,
-                                      mlp_layers=[512, 512])
+                                      mlp_layers=[512, 1024, 512])
             normalize(env, self.dqn_agent, 1000)
             self.sess.run(tf.compat.v1.global_variables_initializer())
 
-        check_point_path = os.path.join(ROOT_PATH, 'self_played_6/tarot_v60073')
+        check_point_path = os.path.join(ROOT_PATH, 'tarot_v60073')
         with self.sess.as_default():
             with self.graph.as_default():
                 saver = tf.compat.v1.train.Saver(tf.compat.v1.model_variables())

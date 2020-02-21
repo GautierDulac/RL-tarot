bbd52bc2eb5f4ce070c430bb8c2bb6557cbe50b5
GD <gautier.dulac@polytechnique.Edu>
2020-02-21 09:22:47 +0100

    ADD - Statistics on model same_played_1

--- examples/statistics.py
+++ examples/statistics.py
@@ -8,15 +8,15 @@ import tensorflow as tf
 import rlcard
 from rlcard.agents.random_agent import RandomAgent
 from rlcard.games.tarot.utils import get_hand_value, get_nb_bouts
-from rlcard.models.pretrained_models_tarot_v_ import TarotDQNModelV1, TarotDQNModelV60073, TarotDQNModelV110017
+from rlcard.models.pretrained_models_tarot_v_ import TarotDQNModelV10075
 from rlcard.utils.logger import Logger
 
 num_tests = 100000
 num_games = 10000
-stats_on_model = 110017
+stats_on_model = 10075
 self_played = True
 self_record_number = 1
-models = {'0': RandomAgent, '1': TarotDQNModelV1, '60073': TarotDQNModelV60073, '110017': TarotDQNModelV110017}
+models = {'0': RandomAgent, '10075': TarotDQNModelV10075}
 
 # Model save path
 save_path = 'examples/statistics/tarot_v{}/'.format(str(stats_on_model))
